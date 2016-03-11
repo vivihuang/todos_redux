e7931342a9937f5939a0e2b58fce866fe5ca71eb
vivihuang <rwhuang@thoughtworks.com>
2016-03-11 17:53:11 +0800

    Finish toggle_todo function

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 
-let TodoList = ({ state }) => {
+let TodoList = ({ state, onClickTodo }) => {
   let list = _.isEmpty(state) ? [] : state
+  let textStyle = (flag) => {
+    return { textDecoration: flag ? 'line-through' : 'none' }
+  }
   return (
     <div>
       <ul>
-        {list.map((s) => { return (<li key={s.id}>{s.text}</li>) })}
+        {list.map((s) => { return (
+          <li key={s.id} style={textStyle(s.completed)} onClick={() => onClickTodo(s.id)}>{s.text}</li>
+        )})}
       </ul>
     </div>
   )
@@ -15,6 +20,19 @@ let TodoList = ({ state }) => {
 
 let mapStateToProps = (state) => { return { state } }
 
-TodoList = connect(mapStateToProps)(TodoList)
+let mapDispatchToProps = (dispatch) => {
+  return { onClickTodo: (id) => dispatch({ type: 'TOGGLE_TODO', id })}
+}
+
+TodoList.propTypes = {
+  state: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  onClickTodo: PropTypes.func.isRequired
+}
+
+TodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList)
 
 export default TodoList
