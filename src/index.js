1f19c23132c95872160bb51442e1e1bf2e748df5
vivihuang <rwhuang@thoughtworks.com>
2016-03-11 16:20:25 +0800

    can add to_do item and show to_do list synchronously

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDom from 'react-dom'
-// import { createStore } from 'redux'
+import { createStore } from 'redux'
+import todos from './reducers/todos'
 
-ReactDom.render(
-  <div>
-    <form onsubmit={ (event) => {
-      event.preventDefault()
+const store = createStore(todos)
+let input
 
-    }} >
-      <input type="text"/>
-      <button>Add todo</button>
-    </form>
-  </div>,
-  document.getElementById('root')
-)
+const render = () => {
+  ReactDom.render(
+    <div>
+      <div>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          let action = {
+            type: 'ADD_TODO',
+            text: input.value
+          }
+          store.dispatch(action)
+          input.value = ''
+        }} >
+          <input type='text' ref={(node) => { input = node }} />
+          <button type='submit'>Add todo</button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          {store.getState().map((s) => {
+            return (<li key={s.id}>{s.text}</li>)
+          })}
+        </ul>
+      </div>
+    </div>,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
